b3e015651281a47cf866dc1fcb3d4ef64c652e93
AsfandYarAhmad <67160854+AsfandYarAhmad@users.noreply.github.com>
2020-06-27 20:52:31 +0500

    create basic react app

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <App name="Asfand Yar Ahmad" age={23} />
+     ,
+     document.getElementById('root')
+   );
+
+// ReactDOM.render(
+//   <div><App/> </div>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
